06dafc3e24acd7e47e247c2e3387fb535529f79a
mavhungutrezzy <adivhahomavhungu@outlook.com>
2024-03-13 16:48:37 +0200

    first commit

---
+++ pomodo_task/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from pomodo_task.users.models import User
+from pomodo_task.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
