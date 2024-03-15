95503f6a031bf800e412928ec6f4d1c9a3285d2d
mavhungutrezzy <adivhahomavhungu@outlook.com>
2024-03-15 16:40:56 +0200

    Refactor: Changed view focus from template to API-centric.

--- pomodo_task/tasks/views.py
+++ pomodo_task/tasks/views.py
@@ -1,51 +1,97 @@
-from django.shortcuts import redirect, render
-from .services import ProjectService, TaskService
-from .forms import ProjectForm, TaskForm
+from rest_framework import viewsets
+from rest_framework.response import Response
+from tasks.serializers import ProjectSerializer, TaskSerializer
+from tasks.services import ProjectService, TaskService
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from core.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import action
 
 
-def create_project(request):
-    projects = ProjectService.get_all_projects()
-    if request.method == "POST":
-        form = ProjectForm(request.POST)
-        if form.is_valid():
-            project = ProjectService.create_project(
-                request.user,
-                form.cleaned_data["name"],
-                form.cleaned_data["description"],
-            )
-            return render(request, "tasks/create_project.html", {"project": project})
-    else:
-        form = ProjectForm()
-
-    return render(
-        request, "tasks/create_project.html", {"form": form, "projects": projects}
-    )
+User = get_user_model()
 
 
-def projects(request):
-    projects = ProjectService.get_all_projects()
-    return render(request, "tasks/projects.html", {"projects": projects})
+class ProjectViewSet(viewsets.ModelViewSet):
+    serializer_class = ProjectSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        user = self.request.user
+        return ProjectService.get_all_projects(owner=user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        ProjectService.create_project(
+            owner=request.user,
+            name=serializer.validated_data["name"],
+            description=serializer.validated_data["description"],
+        )
+        headers = self.get_success_headers(serializer.data)
+        return Response(status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        ProjectService.update_project(
+            instance,
+            serializer.validated_data["name"],
+            serializer.validated_data["description"],
+        )
+        return Response(status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        ProjectService.delete_project(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=True, methods=["get"])
+    def tasks(self, request, pk):
+        project = self.get_object()
+        tasks = ProjectService.get_all_tasks_in_project(project)
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
 
 
-def create_task(request):
-    if request.method == "POST":
-        project = ProjectService.get_project_by_id(request.POST.get("project"))
-        data = {
-            "name": request.POST.get("name"),
-            "description": request.POST.get("description"),
-            "due_date": request.POST.get("due_date"),
-            "priority": request.POST.get("priority"),
-            "project": project,
-            "user": request.user,
-        }
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
 
-        TaskService.create_task(**data)
+    def get_queryset(self):
+        owner = self.request.user
+        return TaskService.get_all_tasks(owner)
 
-        return redirect("tasks:projects")
-    else:
-        form = TaskForm()
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        TaskService.create_task(
+            owner=request.user,
+            name=serializer.validated_data["name"],
+            description=serializer.validated_data["description"],
+            due_date=serializer.validated_data["due_date"],
+            priority=serializer.validated_data["priority"],
+            project=serializer.validated_data["project"],
+        )
+        headers = self.get_success_headers(serializer.data)
+        return Response(status=status.HTTP_201_CREATED, headers=headers)
 
-    projects = ProjectService.get_all_projects()
-    return render(
-        request, "tasks/create_task.html", {"form": form, "projects": projects}
-    )
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        TaskService.update_task(
+            instance,
+            serializer.validated_data["name"],
+            serializer.validated_data["description"],
+            serializer.validated_data["due_date"],
+            serializer.validated_data["priority"],
+            serializer.validated_data["project"],
+        )
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        TaskService.delete_task(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
